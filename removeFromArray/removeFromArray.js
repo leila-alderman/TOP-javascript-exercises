9215643362fb4f676c20a9a848fccdf9246fb0e9
leila-alderman <leila.alderman@gmail.com>
2018-11-30 15:27:55 -0500

    Solve removeFromArray exercise

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr) {
+    let argsToRemove = Array.prototype.slice.call(arguments, 1);
+    let cleanArr = [];
+    outer: for (let i=0; i < arr.length; i++) {
+        for (let j=0; j < argsToRemove.length; j++) {
+            if (arr[i] === argsToRemove[j]) {
+                continue outer;
+            } 
+        }
+        // Push to the array in the outer loop to ensure that all of the checks happen. 
+        cleanArr.push(arr[i]);
+        } 
+    return cleanArr;
 }
 
 module.exports = removeFromArray
