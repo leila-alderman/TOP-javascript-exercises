d450e3304830b88e5391e1a9f93988cedb02b7d0
leila-alderman <leila.alderman@gmail.com>
2018-11-30 12:36:04 -0500

    Solve repeatString exercise

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+  let final = ''
+  if (num < 0) {
+      return 'ERROR';
+  } else {
+    for (let i=0; i<num; i++) {
+        final += str;
+    }
+    return final;
+  }
 }
 
 module.exports = repeatString
