fa99a9459ee84a54acb728c7eb46a61de8bdcb6c
leila-alderman <leila.alderman@gmail.com>
2018-11-30 15:53:59 -0500

    Solve tempConversion exercise

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(tempF) {
+  let tempC = (tempF - 32) * (5/9);
+  tempCRound = Number(tempC.toFixed(1)); 
+  return tempCRound;
 }
 
-const ctof = function() {
-
+const ctof = function(tempC) {
+  let tempF = (tempC * (9/5)) + 32;
+  tempFRound = Number(tempF.toFixed(1));
+  return tempFRound;
 }
 
 module.exports = {
