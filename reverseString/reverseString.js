4d20834351ac372a3658cef9be77616bbe3f2670
leila-alderman <leila.alderman@gmail.com>
2018-11-30 14:24:56 -0500

    Solve reverseString exercise

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    charArr = str.split('');
+    reverseCharArr = [];
+    for (let i=0; i < charArr.length; i++) {
+        reverseCharArr.push(charArr[(charArr.length -1) - i]);
+    }
+    reverseStr = reverseCharArr.join('');
+    return reverseStr;
 }
 
 module.exports = reverseString
