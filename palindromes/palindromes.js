853bb851fd7ed9a2cbf89b9609b5b5b91345845c
leila-alderman <leila.alderman@gmail.com>
2018-12-08 16:51:22 -0500

    Solve palindrome exercise

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,24 @@
-const palindromes = function() {
+const palindromes = function(phrase) {
+    let phraseArr = phrase.toLowerCase().split("");
+    let punctuation = [' ', '.', '?', '!', ','];
+    let cleanPhraseArr = []
+    for (let i=0; i<phrase.length; i++) {
+        if (!punctuation.includes(phraseArr[i])) {
+            cleanPhraseArr.push(phraseArr[i]);
+        }
+    }
 
+    phraseArrBackwards = [];
+    for (let i=cleanPhraseArr.length-1; i >= 0; i--) {
+        phraseArrBackwards.push(cleanPhraseArr[i]);
+    }
+
+    let cleanPhrase = cleanPhraseArr.join("");
+    let phraseBackwards = phraseArrBackwards.join("");
+
+    console.log(cleanPhraseArr)
+    console.log(phraseArrBackwards);
+    return cleanPhrase === phraseBackwards;
 }
 
 module.exports = palindromes
