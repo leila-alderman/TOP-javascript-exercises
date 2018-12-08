8725c8da89434cecd58894f32954406280eba815
leila-alderman <leila.alderman@gmail.com>
2018-12-08 16:11:53 -0500

    Solve calculator exercise

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,25 +1,38 @@
-function add () {
-	
+function add (num1, num2) {
+	return num1 + num2;
 }
 
-function subtract () {
-	
+function subtract (num1, num2) {
+	return num1 - num2;
 }
 
-function sum () {
-	
+function sum (arr) {
+	const total = arr.reduce( (accumulator, currentValue) => {
+		return accumulator + currentValue;
+	}, 0);
+	return total;
 }
 
-function multiply () {
-	
+function multiply (arr) {
+	const total = arr.reduce( (accumulator, currentValue) => {
+		return accumulator * currentValue;
+	});
+	return total;
 }
 
-function power() {
-	
+function power(num1, num2) {
+	return num1 ** num2;	
 }
 
-function factorial() {
-	
+function factorial(num) {
+	if (num === 0 || num === 1) {
+		return 1;
+	} else {
+		for (let i=num-1; i >= 1; i--) {
+			num *= i;
+		}
+		return num;	
+	}
 }
 
 module.exports = {
