05f7f83e558487bd0a1510f1b27747120560cd1a
leila-alderman <leila.alderman@gmail.com>
2018-12-08 17:28:36 -0500

    Solve snakeCase exercise

--- snakeCase/snakeCase.js
+++ snakeCase/snakeCase.js
@@ -1,5 +1,11 @@
-const snakeCase = function() {
-
+const snakeCase = function(string) {
+    let snakeString = string.toLowerCase()
+            .replace(/-/g, ' ')
+            .replace(/\.\./g, ' ')
+            .trim()
+            .replace(/ /g, '_')
+            .replace(/[^_A-Za-z]/g, '');
+    return snakeString;
 }
 
 module.exports = snakeCase
