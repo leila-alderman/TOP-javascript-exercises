ddef7e7fe076b7a4a7473887010eb9336c033b8a
leila-alderman <leila.alderman@gmail.com>
2018-12-08 19:25:18 -0500

    Solve fibonacci exercise

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(index) {
+    index = Number(index);
+    if (index <= 0) {
+        return 'OOPS';
+    }
+    sequence = [1, 1];
+    for (i=2; i < index; i++) {
+        sequence.push(sequence[i-2] + sequence[i-1]);
+    }
+    return sequence[index-1];
 }
 
 module.exports = fibonacci
