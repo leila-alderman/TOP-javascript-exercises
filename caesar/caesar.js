aeda22093f83ac359ed5db85ffab2377c4150005
leila-alderman <leila.alderman@gmail.com>
2018-12-08 19:15:47 -0500

    Solve caesar exercise

--- caesar/caesar.js
+++ caesar/caesar.js
@@ -1,5 +1,37 @@
-const caesar = function() {
+const caesar = function(string, shift) {
+    const alphabetLower = Array.from(Array(26), (e, i) => String.fromCharCode(i + 97));    
+    const alphabetUpper = Array.from(Array(26), (e, i) => String.fromCharCode(i + 65));
+    
+    let stringArr = string.split("")
+    let shiftArr = [];
 
+    function circleBack(num) {
+        if (num >= 26) {
+            return circleBack(num - 26);
+        } else if (num < 0) {
+            return circleBack(num + 26);
+        } else {
+            return num;
+        }
+    }    
+    for (i=0; i < stringArr.length; i++) {
+        if (alphabetLower.includes(stringArr[i])) {
+            let shiftIndex = alphabetLower.indexOf(stringArr[i]) + shift;
+            if (shiftIndex >= 26 || shiftIndex < 0) {
+                shiftIndex = circleBack(shiftIndex);
+            }
+            shiftArr.push(alphabetLower[shiftIndex]);
+        } else if (alphabetUpper.includes(stringArr[i])) {
+            let shiftIndex = alphabetUpper.indexOf(stringArr[i]) + shift;
+            if (shiftIndex >= 26 || shiftIndex < 0) {
+                shiftIndex = circleBack(shiftIndex);
+            }
+            shiftArr.push(alphabetUpper[shiftIndex]);
+        } else {
+            shiftArr.push(stringArr[i]);
+        }        
+    };
+    return shiftArr.join('');
 }
 
 module.exports = caesar
