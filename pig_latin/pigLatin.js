9f807a779f177e656caadcf61bfbaba172cfb473
leila-alderman <leila.alderman@gmail.com>
2018-12-08 20:17:01 -0500

    Solve pigLatin exercise

--- pig_latin/pigLatin.js
+++ pig_latin/pigLatin.js
@@ -1,5 +1,22 @@
-function translate() {
-	// body...
+function translate(string) {
+	let vowels = ['a', 'e', 'i', 'o', 'u']
+	let words = string.split(" ");
+	for (let i=0; i<words.length; i++) {
+		if (vowels.includes(words[i][0])) {
+			words[i] = words[i].concat('ay')
+		} else if (words[i].indexOf("qu") === 0) {
+			words[i] = words[i].slice(2).concat(words[i].slice(0,2)).concat('ay')		
+		} else if (vowels.includes(words[i][1])) {
+			words[i] = words[i].slice(1).concat(words[i][0]).concat('ay')
+		} else if (words[i].indexOf("qu") === 1) {
+			words[i] = words[i].slice(3).concat(words[i].slice(0,3)).concat('ay')
+		} else if (vowels.includes(words[i][2])) {
+			words[i] = words[i].slice(2).concat(words[i].slice(0,2)).concat('ay')
+		} else if (vowels.includes(words[i][3])) {
+			words[i] = words[i].slice(3).concat(words[i].slice(0,3)).concat('ay')
+		}
+	}	
+	return words.join(" ");
 }
 
 
